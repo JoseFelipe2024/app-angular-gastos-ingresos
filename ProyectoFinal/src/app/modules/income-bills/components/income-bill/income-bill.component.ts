a98d6ba7fdf9706798743f7102dcbbd4031283a1
Erinxon <30541728+Erinxon@users.noreply.github.com>
2022-06-02 09:38:11 -0400

    add filter

--- ProyectoFinal/src/app/modules/income-bills/components/income-bill/income-bill.component.ts
+++ ProyectoFinal/src/app/modules/income-bills/components/income-bill/income-bill.component.ts
@@ -3,6 +3,7 @@ import { TransactionBaseService } from 'src/app/core/services/transactionBase.se
 import { ApiResponse } from 'src/app/shared/models/apiResponse.model';
 import { Transaction } from 'src/app/shared/models/transaction.mode';
 import { TransactionType } from 'src/app/shared/models/transaction-Type.model';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-income-bill',
@@ -12,6 +13,21 @@ import { TransactionType } from 'src/app/shared/models/transaction-Type.model';
 })
 export class IncomeBillComponent implements OnInit {
   transaction: Transaction[] = [];
+  transactionOriginalList: Transaction[] = [];
+  selectedValue: any;
+
+  typesTransaction: any[] = [
+    {
+      id: TransactionType.Income,
+      name: 'Ingreso'
+    },
+    {
+      id: TransactionType.Bill,
+      name: 'Gasto'
+    },
+  ];
+  amount: number = 0;
+  date!: Date;
 
   constructor(private transactionBaseService: TransactionBaseService) { }
 
@@ -22,6 +38,7 @@ export class IncomeBillComponent implements OnInit {
   getTransactions(){
     this.transactionBaseService.getTransactions().subscribe((res: ApiResponse<Transaction[]>) => {
       this.transaction = res.data;
+      this.transactionOriginalList = res.data;
     }, error => {
       console.log(error);
     });
@@ -33,6 +50,23 @@ export class IncomeBillComponent implements OnInit {
 
   getClassButtonEvidence(typeFile: string){
     return typeFile === 'application/pdf' ? 'pi-file-pdf' : 'pi-image';
+  }
+
+  filter(){
+    /*if(!this.date && this.amount === 0 && !this.selectedValue){
+      this.transaction = [...this.transactionOriginalList];
+      return;
+    }*/
+    console.log(this.selectedValue, this.amount, this.date)
+    let transaction = this.transactionOriginalList.filter(t => t.type === this.selectedValue
+      || this.getFormatDate(this.date) === this.getFormatDate(t.date) || this.amount === t.amount);
+      console.log(transaction)
+    this.transaction = transaction;
+  }
+
+  getFormatDate(date: any){
+    if(!date) return;
+    return formatDate(date, 'MM-yyyy-dd', 'en-US');
   }
 
 }
--- ProyectoFinal/src/app/modules/income-bills/income-bill.module.ts
+++ ProyectoFinal/src/app/modules/income-bills/income-bill.module.ts
@@ -5,10 +5,17 @@ import { ButtonModule } from "primeng/button";
 import { CoreModule } from "src/app/core/core.module";
 import { IncomeBillComponent } from "./components/income-bill/income-bill.component";
 import { IncomeBillRoutingModule } from "./income-bill-Routing.module";
+import {MatSelectModule} from '@angular/material/select';
+import { FormsModule } from "@angular/forms";
+import {DropdownModule} from 'primeng/dropdown';
+import {CalendarModule} from 'primeng/calendar';
+import {InputNumberModule} from 'primeng/inputnumber';
 
 @NgModule({
     declarations: [IncomeBillComponent],
-    imports: [IncomeBillRoutingModule, ButtonModule, MatTooltipModule, CommonModule, CoreModule],
+    imports: [IncomeBillRoutingModule, FormsModule, 
+      ButtonModule, DropdownModule, CalendarModule,
+       MatTooltipModule, CommonModule, CoreModule, InputNumberModule],
     providers: [],
     bootstrap: [IncomeBillModule],
   })
