3033214718d9fde102574814d08b685207614683
Erinxon Santana <erinxons@gmail.com>
2022-09-19 13:15:56 -0400

    fix

--- Proyecto_final.Server/Startup.cs
+++ Proyecto_final.Server/Startup.cs
@@ -32,7 +32,6 @@ namespace Proyecto_final.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddCors(options => options.AddPolicy("ApiCorsPolicy", builder =>
             {
