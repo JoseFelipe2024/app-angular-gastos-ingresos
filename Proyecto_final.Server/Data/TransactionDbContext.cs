b85687484e6f2fba4c22999d5a0d78f847eb0bb8
Erinxon Santana <erinxons@gmail.com>
2023-07-06 22:44:17 -0400

    change

--- Proyecto_final.Server/Data/TransactionDbContext.cs
+++ Proyecto_final.Server/Data/TransactionDbContext.cs
@@ -11,6 +11,11 @@ namespace Proyecto_final.Server.Data
     {
         public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
         {
+            if (!this.Database.GetPendingMigrations().Any())
+            {
+                this.Database.Migrate();
+            }
+         
         }
 
         public DbSet<Transaction> Transaction { get; set; }
